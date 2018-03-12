0ea6c292c69a634336db10e850b73d2a83647198
Alexandre Boudnik <Alexandre_Boudnik@epam.com>
2018-03-12 15:40:44 -0400

    lambdas for transactions

--- src/test/java/org/boudnik/framework/test/Main.java
+++ src/test/java/org/boudnik/framework/test/Main.java
@@ -2,28 +2,64 @@ package org.boudnik.framework.test;
 
 import org.boudnik.framework.Store;
 import org.boudnik.framework.Transaction;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import sun.plugin2.jvm.CircularByteBuffer;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Alexandre_Boudnik
  * @since 02/13/2018
  */
 public class Main {
+
     @Test
     public void main() {
-        Store store = Store.instance();
-        store.create(TestEntry.class);
-        try (Transaction tx = store.begin()) {
+        Store.instance().create(TestEntry.class);
+        try (Transaction tx = Store.instance().begin()) {
             new TestEntry("http://localhost/1").save("");
             tx.commit();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        try (Transaction tx = store.begin()) {
+        try (Transaction tx = Store.instance().begin()) {
             TestEntry entry = tx.get(TestEntry.class, "http://localhost/1");
             System.out.println("entry = " + entry);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void mt() {
+        Store.instance().create(TestEntry.class);
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        executor.submit(() -> {
+            try (Transaction tx = Store.instance().begin()) {
+                new TestEntry("http://localhost/1").save("");
+                System.out.println("tx = " + tx);
+                tx.commit();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        executor.submit(() -> {
+            try (Transaction tx = Store.instance().begin()) {
+                new TestEntry("http://localhost/1").save("");
+                System.out.println("tx = " + tx);
+                tx.commit();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        try {
+            executor.awaitTermination(2, TimeUnit.SECONDS);
+        } catch (InterruptedException ignored) {
         }
     }
 }
